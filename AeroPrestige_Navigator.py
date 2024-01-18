8941248fdb9fde872539a07e5c5299377e2c0efd
Yueyin Ji <146648308+Jurin57@users.noreply.github.com>
2024-01-18 16:20:11 +0800

    Update AeroPrestige_Navigator.py

--- AeroPrestige_Navigator.py
+++ AeroPrestige_Navigator.py
@@ -1,7 +1,6 @@
-# PART A ----------------------------------------------------------------------------------
-print('Welcome to the Python Airlines Prototype Membership System.\n')
+print('Welcome to the Airlines Prototype Membership System.\n')
 
-# PART B ----------------------------------------------------------------------------------
+# customers' information input-------------------------------------------------------------
 # Ask the user to input information about themselves
 FirstName = input('What is your first name?')
 AnnualMiles = input('How many miles have you flown this year?')
@@ -11,7 +10,6 @@ LifetimeMiles = input('How many lifetime miles have you flown?')
 LoungeCheck = input('Is your next flight to(or through) the Chicago airport?')
 TicketPrice = input('How much will a Coach Class ticket for your next flight cost(USD)?')
 
-# PART C ----------------------------------------------------------------------------------
 # Convert the inputs to the appropriate data type
 FirstName = str(FirstName)
 AnnualMiles = int(AnnualMiles)
@@ -20,10 +18,12 @@ AnnualDollars = float(AnnualDollars)
 LifetimeMiles = int(LifetimeMiles)
 LoungeCheck = bool(LoungeCheck)
 TicketPrice = float(TicketPrice)
+input_summary = '\n{},this year, you have flown {:,} flights for {:,} miles and spent ${:,.2f} USD with Airlines. Lifetime, you have flown {:,} miles.\n'
+print(input_summary.format(FirstName, AnnualSegments, AnnualMiles, AnnualDollars, LifetimeMiles))
 
-# PART D ----------------------------------------------------------------------------------
+
+# Build the customer segmentation system---------------------------------------------------
 # Assign the user the appropriate tier base on the criteria
-
 annual_membership_tier = ''
 lifetime_membership_tier = ''
 
@@ -35,7 +35,7 @@ elif (AnnualMiles >= 75000 and AnnualMiles < 100000) or ((AnnualSegments >=90 an
     annual_membership_tier = 'Ruby'
 elif AnnualMiles >= 100000 or (AnnualSegments >=120 and AnnualDollars >= 24000):
     annual_membership_tier = 'Diamond'
-
+    
 if LifetimeMiles >= 1000000 and LifetimeMiles < 2000000:
     lifetime_membership_tier = 'Emerald'
 elif LifetimeMiles >= 2000000 and LifetimeMiles < 3000000:
@@ -43,11 +43,37 @@ elif LifetimeMiles >= 2000000 and LifetimeMiles < 3000000:
 elif LifetimeMiles >= 3000000:
     lifetime_membership_tier = 'Diamond'
 
+# display the customer's membership status
+display_membership = ''
+annual_membership_class = 0
+lifetime_membership_class = 0
+
+# A dictionary mapping membership tier names to their respective class levels
+membership_tiers = {'Sapphire': 1, 'Emerald': 2, 'Ruby': 3, 'Diamond': 4}
+
+# Retrieve the class level for annual and lifetime membership tiers
+# If the tier is not found, it defaults to 0 (which indicates no status)
+annual_membership_class = membership_tiers.get(annual_membership_tier, 0)
+lifetime_membership_class = membership_tiers.get(lifetime_membership_tier, 0)
+
+if lifetime_membership_class != 0:
+    if annual_membership_class >= lifetime_membership_class:
+        display_membership = 'You have achieved life time frequent-flier status at the {} level!\nThis year, you have also achieved annual frequent-flier status at the {} level!'.format(lifetime_membership_tier, annual_membership_tier)
+    elif annual_membership_class < lifetime_membership_class:
+        display_membership = 'This year, you have achieved annual frequent-flier status at the {} level!'.format(annual_membership_tier)
+elif lifetime_membership_class == 0:
+    if annual_membership_class != 0:
+        display_membership = 'This year, you have achieved annual frequent-flier status at the {} level!'.format(annual_membership_tier)
+    else:
+        display_membership = 'You are on your way to achieving annual frequent-flier status with Python Airlines!'
+
+print(display_membership + '\n')
 
 
-# PART E ----------------------------------------------------------------------------------
+
+# Build the rewards system-----------------------------------------------------------------
+
 # Determine the user's rewards based on the highest memberhsip tier
-
 rewards_bags = 0
 rewards_upgrades = ''
 rewards_lounge = False
@@ -78,38 +104,7 @@ elif annual_membership_tier == 'Diamond':
     rewards_lounge = True
     rewards_discount = 0.30
 
-
-# PART F ----------------------------------------------------------------------------------
-input_summary = '\n{},this year, you have flown {:,} flights for {:,} miles and spent ${:,.2f} USD with Python Airlines. Lifetime, you have flown {:,} miles.\n'
-print(input_summary.format(FirstName, AnnualSegments, AnnualMiles, AnnualDollars, LifetimeMiles))
-
-# PART G ----------------------------------------------------------------------------------
-display_membership = ''
-annual_membership_class = 0
-lifetime_membership_class = 0
-
-# A dictionary mapping membership tier names to their respective class levels
-membership_tiers = {'Sapphire': 1, 'Emerald': 2, 'Ruby': 3, 'Diamond': 4}
-
-# Retrieve the class level for annual and lifetime membership tiers
-# If the tier is not found, it defaults to 0 (which indicates no status)
-annual_membership_class = membership_tiers.get(annual_membership_tier, 0)
-lifetime_membership_class = membership_tiers.get(lifetime_membership_tier, 0)
-
-if lifetime_membership_class != 0:
-    if annual_membership_class >= lifetime_membership_class:
-        display_membership = 'You have achieved life time frequent-flier status at the {} level!\nThis year, you have also achieved annual frequent-flier status at the {} level!'.format(lifetime_membership_tier, annual_membership_tier)
-    elif annual_membership_class < lifetime_membership_class:
-        display_membership = 'This year, you have achieved annual frequent-flier status at the {} level!'.format(annual_membership_tier)
-elif lifetime_membership_class == 0:
-    if annual_membership_class != 0:
-        display_membership = 'This year, you have achieved annual frequent-flier status at the {} level!'.format(annual_membership_tier)
-    else:
-        display_membership = 'You are on your way to achieving annual frequent-flier status with Python Airlines!'
-
-print(display_membership + '\n')
-
-# PART H ----------------------------------------------------------------------------------
+# display the customer's rewards
 # Initialize empty strings for various display messages related to rewards
 display_reward_summary = ''
 display_reward_upgrades = ''
@@ -134,14 +129,6 @@ if annual_membership_class != 0:
         display_reward_lounge = '- Enjoy free access to the club lounge in Chicago on your next/upcoming flight!' if LoungeCheck else '- Free access to the club lounge next time flying to/through in Chicago.'
 
 print(display_reward_summary + display_reward_bag + display_reward_upgrades + display_reward_discount + display_reward_lounge)
-        
-
-
-
-
-
-
-
 
 
 
